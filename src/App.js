4e96717980a4130d022cb1fda8021cc2be74dc3a
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-10-01 09:55:11 -0400

    4th. commit: Index & App components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import HelloContainer from './components/HelloContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link to="/greetings">Link to Greetings</Link>
+      <Routes>
+        <Route path="/greetings" element={<HelloContainer />} />
+      </Routes>
+    </>
   );
 }
 
